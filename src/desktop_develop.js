98fc96672ca47b9e8739fd678c94c55041c1a74f
David Baker <dave@matrix.org>
2020-03-03 13:53:38 +0000

    Building on windows (64-bit)
    With virtualbox automation and windows batch file scripting :/

--- src/desktop_develop.js
+++ src/desktop_develop.js
@@ -25,7 +25,9 @@ const logger = require('./logger');
 const Runner = require('./runner');
 const DockerRunner = require('./docker_runner');
 
-const TYPES = ['mac', 'linux'];
+const WindowsBuilder = require('./windows_builder');
+
+const TYPES = ['win64', 'mac', 'linux'];
 
 const DESKTOP_GIT_REPO = 'https://github.com/vector-im/riot-desktop.git';
 const ELECTRON_BUILDER_CFG_FILE = 'electron-builder.json';
@@ -62,15 +64,24 @@ async function putLastBuildTime(type, t) {
 }
 
 function getBuildVersion() {
-    // YYYYMMDDNNN where NNN is in case we need to do multiple versions in a day
+    // YYYYMMDDNN where NN is in case we need to do multiple versions in a day
+    // NB. on windows, squirrel will try to parse the versiopn number parts,
+    // including this string, into 32 bit integers, which is fine as long
+    // as we only add two digits to the end...
     const now = new Date();
     const month = (now.getMonth() + 1).toString().padStart(2, '0');
-    const date = (now.getDate() + 1).toString().padStart(2, '0');
-    const buildNum = '001';
+    const date = now.getDate().toString().padStart(2, '0');
+    const buildNum = '01';
     return now.getFullYear() + month + date + buildNum;
 }
 
 class DesktopDevelopBuilder {
+    constructor(winVmName, winUsername, winPassword) {
+        this.winVmName = winVmName;
+        this.winUsername = winUsername;
+        this.winPassword = winPassword;
+    }
+
     async start() {
         logger.info("Starting Desktop/develop builder...");
         this.building = false;
@@ -122,9 +133,17 @@ class DesktopDevelopBuilder {
             extraMetadata: {
                 name: "riot-desktop-nightly",
                 productName: "Riot Nightly",
-                version: buildVersion,
+                // the windows packager relies on parsing this as semver :(
+                version: '0.0.0-nightly.' + buildVersion,
             },
             appId: "im.riot.nightly",
+            win: Object.assign({
+                // We keep this in here so the riot-desktop repo will build
+                // for windows unsigned by default and not error if you don't
+                // have our cert installed, which means others can build it
+                // without having to patch package.json
+                certificateSubjectName: "New Vector Ltd",
+            }, cfg.win),
         });
         await fsProm.writeFile(
             path.join(repoDir, ELECTRON_BUILDER_CFG_FILE),
@@ -133,6 +152,14 @@ class DesktopDevelopBuilder {
     }
 
     async build(type, buildVersion) {
+        if (type.startsWith('win')) {
+            return this.buildWin(type, buildVersion);
+        } else {
+            return this.buildLocal(type, buildVersion);
+        }
+    }
+
+    async buildLocal(type, buildVersion) {
         const repoDir = 'riot-desktop-' + type + '-' + buildVersion;
         await new Promise((resolve, reject) => {
             rimraf(repoDir, (err) => {
@@ -144,6 +171,7 @@ class DesktopDevelopBuilder {
         await repo.clone(DESKTOP_GIT_REPO, repoDir);
         // NB. we stay on the 'master' branch of the riot-desktop
         // repo (and fetch the develop version of riot-web later)
+        //await repo.checkout('');
         logger.info("...checked out 'develop' branch, starting build for " + type);
 
         await this.writeElectronBuilderConfigFile(repoDir, buildVersion);
@@ -156,13 +184,9 @@ class DesktopDevelopBuilder {
             case 'linux':
                 runner = this.makeLinuxRunner(repoDir);
                 break;
-            case 'win':
-                runner = this.makeWinRunner(repoDir);
-                break;
         }
 
-        await this.buildWithRunner(runner, buildVersion);
-
+        await this.buildWithRunner(runner, buildVersion, type);
         logger.info("Build completed!");
     }
 
@@ -174,15 +198,63 @@ class DesktopDevelopBuilder {
         return new DockerRunner(cwd, path.join('scripts', 'in-docker.sh'));
     }
 
-    async buildWithRunner(runner, buildVersion) {
+    async buildWithRunner(runner, buildVersion, type) {
         await runner.run('yarn', 'install');
         await runner.run('yarn', 'run', 'hak', 'check');
-        //await runner.run('yarn', 'run', 'build:native');
+        await runner.run('yarn', 'run', 'build:native');
         await runner.run('yarn', 'run', 'fetch', 'develop', '-d', 'riot.im');
-        // This part only actually necessary for the Debian package, but no harm
-        // in doing it for other platforms
-        await runner.run('scripts/set-version.js', '--deb', buildVersion);
+        // This part only actually necessary for the Debian package
+        if (type == 'linux') {
+            await runner.run('scripts/set-version.js', '--deb', buildVersion);
+        }
         await runner.run('yarn', 'build', '--config', ELECTRON_BUILDER_CFG_FILE);
+    }
+
+    async buildWin(type, buildVersion) {
+        const repoDir = 'riot-desktop-' + type + '-' + buildVersion;
+        await new Promise((resolve, reject) => {
+            rimraf(repoDir, (err) => {
+                err ? reject(err) : resolve();
+            });
+        });
+
+        // we still check out the repo locally because we need package.json
+        // to write the electron builder config file, so we check out the
+        // repo twice for windows: once locally and once on the VM...
+        const repo = new GitRepo(repoDir);
+        await repo.clone(DESKTOP_GIT_REPO, repoDir);
+        //await fsProm.mkdir(repoDir);
+        await this.writeElectronBuilderConfigFile(repoDir, buildVersion);
+
+        const builder = new WindowsBuilder(
+            repoDir, type, this.winVmName, this.winUsername, this.winPassword,
+        );
+
+        console.log("Starting Windows builder for " + type);
+        await builder.start();
+
+        const electronBuilderArchFlag = type === 'win64' ? '--x64' : '--ia32';
+
+        try {
+            builder.appendScript('rd', repoDir, '/s', '/q');
+            builder.appendScript('git', 'clone', DESKTOP_GIT_REPO, repoDir);
+            builder.appendScript('cd', repoDir);
+            builder.appendScript('copy', 'z:\\' + ELECTRON_BUILDER_CFG_FILE, ELECTRON_BUILDER_CFG_FILE);
+            builder.appendScript('call', 'yarn', 'install');
+            builder.appendScript('call', 'yarn', 'run', 'hak', 'check');
+            builder.appendScript('call', 'yarn', 'run', 'build:native');
+            builder.appendScript('call', 'yarn', 'run', 'fetch', 'develop', '-d', 'riot.im');
+            builder.appendScript('call', 'yarn', 'build', electronBuilderArchFlag, '--config', ELECTRON_BUILDER_CFG_FILE);
+            builder.appendScript('xcopy dist z:\\dist /S /I /Y');
+            builder.appendScript('cd', '..');
+            builder.appendScript('rd', repoDir, '/s', '/q');
+
+            console.log("Starting build...");
+            await builder.runScript();
+            console.log("Build complete!");
+        } finally {
+            await builder.stop();
+        }
     }
 }
 
